865dfdd9cfabdd02c5b0640211d6dfc6acc49c05
Daniel <3573053@gmail.com>
2023-07-05 09:29:54 +1200

    init

---
+++ src/app/(root)/layout.jsx
@@ -0,0 +1,8 @@
+export default function layout({children}) {
+  return (
+    <div>
+      <h1>ROOT LAY</h1>
+      <div>CHILD: {children}</div>
+    </div>
+  )
+}
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,7 @@
+export default function OnePage() {
+  return (
+    <div data-ui={'OnePage'}>
+      Dashboard
+    </div>
+  )
+}
