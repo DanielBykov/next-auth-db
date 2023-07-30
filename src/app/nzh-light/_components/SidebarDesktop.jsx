e59f24790ef76a75db69399a5483f898edb8e261
Daniel Bykov <daniel.bykov@nzme.co.nz>
2023-07-31 10:08:32 +1200

    topic menu update

--- src/app/nzh-light/_components/SidebarDesktop.jsx
+++ src/app/nzh-light/_components/SidebarDesktop.jsx
@@ -1,8 +1,6 @@
 "use client"
 import Link from "next/link";
-import {Cog6ToothIcon, HomeIcon, UsersIcon, BriefcaseIcon, GlobeAltIcon, BellIcon, HomeModernIcon, CursorArrowRaysIcon,
-GlobeEuropeAfricaIcon, TrophyIcon, ChatBubbleBottomCenterIcon, XMarkIcon
-} from "@heroicons/react/24/outline";
+import {Cog6ToothIcon, XMarkIcon, PlusIcon} from "@heroicons/react/24/outline";
 import {useState} from "react";
 import {initTopics} from "@/app/nzh-light/_components/mock-data";
 import {usePathname} from "next/navigation";
@@ -13,16 +11,20 @@ function classNames(...classes) {
 }
 const urlSegment = '/nzh-light'
 
-export const SidebarDesktop = ({navigation, teams}) => {
+export const SidebarDesktop = () => {
   let path = usePathname()
+  let currentTopicUrl = path.split('/')[2]
 
   const [topics, setTopics ] = useState(initTopics)
   const [favTopics, setFavTopics ] = useState([])
-  const topicClick = (item) => () => {
+
+  const topicClick = (item) => (e) => {
+    e.preventDefault()
     setFavTopics([...favTopics, item])
     setTopics([...topics.filter((item_) => item_ !== item)])
   }
-  const favTopicClick = (item) => () => {
+  const favTopicClick = (item) => (e) => {
+    e.preventDefault()
     setFavTopics([...favTopics.filter((item_) => item_ !== item)])
     setTopics([...topics, item])
   }
@@ -43,24 +45,21 @@ export const SidebarDesktop = ({navigation, teams}) => {
 
                 {favTopics.sort((a,b) => a.id-b.id).map((item) => (
                   <li key={item.name}>
-                    <div
+                    <Link href={`${urlSegment}/${item.href}`}
                       className={classNames(
-                        `/nzh-light/${item.href}` === path
+                        item.href === currentTopicUrl
                           ? 'bg-yellow-600 text-white'
                           : 'text-indigo-200 hover:text-white hover:bg-yellow-600/20',
                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold cursor-pointer'
                       )}
                     >
-                      <Link className="flex gap-3" href={`/nzh-light/${item.href}`}>
+                      <div className="flex gap-3">
                         <item.icon
-                          className={classNames(
-                            item.current ? 'text-white' : 'text-yellow-400 group-hover:text-white',
-                            'h-6 w-6 shrink-0'
-                          )}
+                          className='text-yellow-400 group-hover:text-white h-6 w-6 shrink-0'
                           aria-hidden="true"
                         />
                         <div className="">{item.name}</div>
-                      </Link>
+                      </div>
 
                       <div className="flex-1 flex justify-end items-center">
                         <XMarkIcon
@@ -70,7 +69,7 @@ export const SidebarDesktop = ({navigation, teams}) => {
                         />
                       </div>
 
-                    </div>
+                    </Link>
                   </li>
                 ))}
               </ul>
@@ -83,25 +82,44 @@ export const SidebarDesktop = ({navigation, teams}) => {
 
                 {topics.sort((a,b) => a.id-b.id).map((item) => (
                   <li key={item.name}>
-                    <div
-                      onClick={topicClick(item)}
+                    <Link
+                      href={`${urlSegment}/${item.href}`}
                       className={classNames(
-                        'text-indigo-200 hover:text-white hover:bg-yellow-600/20',
+                        item.href === currentTopicUrl
+                          ? 'bg-yellow-600 text-white'
+                          : 'text-indigo-200 hover:text-white hover:bg-yellow-600/20',
+
                         'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold cursor-pointer'
                       )}
                     >
-                      <item.icon
-                        className={classNames(
-                          item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white',
-                          'h-6 w-6 shrink-0'
-                        )}
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </div>
+                      <div className="flex gap-3">
+                        <item.icon
+                          className={classNames(
+                            // item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white',
+                            'h-6 w-6 shrink-0'
+                          )}
+                          aria-hidden="true"
+                        />
+                        {item.name}
+                      </div>
+
+                      <div className="flex-1 flex justify-end items-center">
+                        <PlusIcon
+                          onClick={topicClick(item)}
+                          className="h-4 w-4 text-white/20 hover:text-white"
+                          aria-hidden="true"
+                        />
+                      </div>
+
+                    </Link>
                   </li>
                 ))}
               </ul>
+            </li>
+            <li>
+              <div className="border border-gray-800 rounded p-2 text-center text-sm">
+                Click "+" to add a topic to the favourite section
+              </div>
             </li>
             <li className="mt-auto">
               <a
