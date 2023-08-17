e1fb0f654ba6a77655ec245c3192bd785ab192d7
Daniel Bykov <daniel.bykov@nzme.co.nz>
2023-08-18 11:32:25 +1200

    login redirect + refactoring

--- src/app/nzh-light/layout.jsx
+++ src/app/nzh-light/layout.jsx
@@ -11,10 +11,11 @@ import { ChevronDownIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import {useSession} from "next-auth/react";
 import {Sidebar} from "@/app/nzh-light/_components/Sidebar";
 import {ToggleDarkTheme} from "@/app/nzh-light/_components/ToggleDarkTheme";
+import Link from "next/link";
 
 const userNavigation = [
   { name: 'Your profile', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Sign out', href: '/api/auth/signout' },
 ]
 
 function classNames(...classes) {
@@ -26,10 +27,6 @@ export default function NZHLayout({children}) {
 
   // const systemDef = window.matchMedia('(prefers-color-scheme: dark)').matches
   const [isDark, setIsDark ] = useState(false)
-
-  const { data: session } = useSession()
-  const userName = session?.user?.name
-  const userImage = session?.user?.image
 
   return (
     <div className={classNames(isDark && "dark", "h-full")}>
@@ -127,49 +124,8 @@ export default function NZHLayout({children}) {
                 <div className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true" />
 
                 {/* Profile dropdown */}
-                <Menu as="div" className="relative">
-                  <Menu.Button className="-m-1.5 flex items-center p-1.5">
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-8 w-8 rounded-full bg-gray-50"
-                      src={userImage}
-                      alt=""
-                    />
-                    <span className="hidden lg:flex lg:items-center">
-                      <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
-                        {userName}
-                      </span>
-                      <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    </span>
-                  </Menu.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                      {userNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          {({ active }) => (
-                            <a
-                              href={item.href}
-                              className={classNames(
-                                active ? 'bg-gray-50' : '',
-                                'block px-3 py-1 text-sm leading-6 text-gray-900'
-                              )}
-                            >
-                              {item.name}
-                            </a>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <ProfileDropdown/>
+
               </div>
             </div>
           </div>
@@ -181,4 +137,60 @@ export default function NZHLayout({children}) {
       </div>
     </div>
   )
+}
+
+const ProfileDropdown = () => {
+  const { data: session } = useSession()
+  const userName = session?.user?.name
+  const userImage = session?.user?.image
+  console.log('d256 session:', session)
+  return session
+    ? <Menu as="div" className="relative">
+          <Menu.Button className="-m-1.5 flex items-center p-1.5">
+            <span className="sr-only">Open user menu</span>
+            <img
+              className="h-8 w-8 rounded-full bg-gray-50"
+              src={userImage}
+              alt=""
+            />
+            <span className="hidden lg:flex lg:items-center">
+            <span className="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">
+              {userName}
+            </span>
+            <ChevronDownIcon className="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
+          </span>
+          </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+              {userNavigation.map((item) => (
+                <Menu.Item key={item.name}>
+                  {({ active }) => (
+                    <Link
+                      href={item.href}
+                      className={classNames(
+                        active ? 'bg-gray-50' : '',
+                        'block px-3 py-1 text-sm leading-6 text-gray-900'
+                      )}
+                    >
+                      {item.name}
+                    </Link>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
+    : <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <Link href="/api/auth/signin" className="text-sm font-semibold leading-6 text-gray-900">
+          Log in <span aria-hidden="true">&rarr;</span>
+        </Link>
+      </div>
 }
